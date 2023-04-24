d8c1194bf7a328d18fc323f91cc09b60af501a0e
vannalyc <126526667+vannalyc@users.noreply.github.com>
2023-04-24 15:12:57 -0700

    Update vowels&const.js

--- vowels&const.js
+++ vowels&const.js
@@ -1,10 +1,11 @@
+//input value 
 const x = 'hello'
 function vowel_count(str1)
 {
+// defining vowels
   var vowel_list = 'aeiouAEIOU';
   var vcount = 0;
-  
-  
+// vowel count
   for(var x = 0; x < str1.length ; x++)
   {
     if (vowel_list.indexOf(str1[x]) !== -1)
@@ -18,4 +19,4 @@ function vowel_count(str1)
 
 }
 
-console.log(vowel_count(x));
+console.log( x + " " + 'has' + " " + vowel_count(x) + " " + 'vowels');
